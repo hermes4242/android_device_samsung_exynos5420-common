92f7f533bad04bdac0781879f617012de00bdc8a
Stefan Hermes <stefan@hermes-net.de>
2020-08-16 16:49:21 +0200

    initial commit from LineageOS copy

---
+++ system_prop.mk
@@ -0,0 +1,27 @@
+#
+# Common system properties for exynos5420
+#
+
+# Media
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.sapa.jack.master=alsa \
+    ro.hdcp2.rx=tz \
+    persist.bluetooth.disableabsvol=true
+
+# Open GL
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.opengles.version=196609
+
+# Randomly from stock
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.ril.gprsclass=10 \
+    ro.ril.hsxpa=1 \
+    ro.sec.fle.encryption=true \
+    ro.secwvk=220
+
+# sdcardfs
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.sys.sdcardfs=true
+
+# AptXHD
+ persist.bt.enableAptXHD=true
